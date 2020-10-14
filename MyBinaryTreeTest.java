4f6e4056d5dbe23fdfd96013700082bf25effbf8
premchandAllu <premchandallu1999@gmail.com>
2020-10-14 09:42:39 +0530

    Added: All Elements to the tree

--- MyBinaryTreeTest.java
+++ MyBinaryTreeTest.java
@@ -4,16 +4,29 @@ import org.junit.jupiter.api.Test;
 
 import junit.framework.Assert;
 
+@SuppressWarnings("deprecation")
 public class MyBinaryTreeTest {
 
 	@Test
-	public void createBinaryTreeAddIntegersTest() {
-		MyBinaryTree<Integer> myBinaryTree=new MyBinaryTree<Integer>();
+  public void createBinaryTreeAddIntegersTest() {
+		
+	MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<Integer>();
+		
 		myBinaryTree.add(56);
 		myBinaryTree.add(30);
 		myBinaryTree.add(70);
-		int size=myBinaryTree.getSize();
-		Assert.assertEquals(3, size);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(11);
+		myBinaryTree.add(65);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+		int size = myBinaryTree.getSize();
+		Assert.assertEquals(13, size);
 	}
 	
 }
