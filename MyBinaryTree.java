63b34c799e83f40efb79bbe0b9fa0914fb9d7553
premchandAllu <premchandallu1999@gmail.com>
2020-10-14 09:33:56 +0530

    Added:Binary search tree

---
+++ MyBinaryTree.java
@@ -0,0 +1,10 @@
+package com.blz.BinarySearchTree;
+
+public class MyBinaryTree {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Binary Search Tree Problem");
+	}
+
+}
